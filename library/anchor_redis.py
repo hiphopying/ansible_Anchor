6882dac9bd949d276e9d7a5a088a231cccc7f93e
hiphopying <sjtuxuyinying@gmail.com>
2017-05-17 07:17:47 +0800

    Initial version:  - Ansible redis module defined. Includeing: 	set 	get 	hmset 	hmget  - Some test playbooks.

---
+++ library/anchor_redis.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: anchor_redis
+short_description: Includes various redis commands.
+description: 
+    - The official redis module only contains 'slave', 'flush', 'config'
+    commands. 
+    - This module contains much more commands like set/get/hmset/hmget etc to
+    interact with redis server.
+author: henryy.xu@dell.com
+options:
+requirements:[redis]
+notes:
+'''
+
+EXAMPLES = '''
+
+'''
+
+try:
+    import redis
+except ImportError:
+    redis_found = False
+else:
+    redis_found = True
+
+def anch_update(client, cmd, **kwargs):
+    if cmd == "set":
+        assert kwargs['key'] != None
+        assert kwargs['value'] != None
+        key = kwargs['key']
+        value = kwargs['value']
+        try:
+            return client.set(key, value)
+        except Exception:
+            raise 
+    elif cmd == "get":
+        assert kwargs['key'] != None
+        key = kwargs['key']
+        try:
+            return client.get(key)
+        except Exception:
+            raise
+    elif cmd == "hmset":
+        assert kwargs['hashkey'] != None
+        assert kwargs['hashvalue'] != None
+        hashkey = kwargs['hashkey']
+        hashvalue = kwargs['hashvalue']
+        try:
+            return client.hmset(hashkey, hashvalue)
+        except Exception:
+            raise
+    elif cmd == "hmget":
+        assert kwargs['hashkey'] != None
+        assert kwargs['hashfield'] != None
+        hashkey = kwargs['hashkey']
+        hashfield = kwargs['hashfield']
+        try:
+            return client.hmget(hashkey, hashfield)
+        except Exception:
+            raise
+
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            login_host=dict(default='localhost'),
+            login_port=dict(default=6379, type='int'),
+            db=dict(default=None, type='int'),
+            command=dict(default=None, choices=['set', 'get', 'hmset', 'hmget']),
+            key=dict(default=None),
+            value=dict(default=None),
+            hashkey=dict(default=None),
+            hashfield=dict(default=None, type='list'),
+            hashvalue=dict(default=None, type='dict')
+        ),
+        supports_check_mode = True
+    )
+
+    if not redis_found:
+        module.fail_json(msg="python redis module is required")
+
+    login_host = module.params['login_host']
+    login_port = module.params['login_port']
+    db = module.params['db']
+    try:
+        pool = redis.ConnectionPool(host=login_host, port=login_port, db=db)
+        r = redis.Redis(connection_pool=pool)
+        r.ping()
+    except Exception:
+       e = get_exception()
+       module.fail_json(msg="Unable to connect to database {0}".format(e))
+
+    command = module.params['command']
+    key = module.params['key']
+    value = module.params['value']
+    hashkey = module.params['hashkey']
+    hashfield = module.params['hashfield']
+    hashvalue = module.params['hashvalue']
+    
+    result = anch_update(r, command, key=key, value=value, hashkey=hashkey,
+                   hashfield=hashfield, hashvalue=hashvalue) 
+    if result and command in ['get', 'hmget']:
+        module.exit_json(changed=False, values=result)
+    else:
+        module.exit_json(changed=True)
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.pycompat24 import get_exception
+
+if __name__ == '__main__':
+     main()
